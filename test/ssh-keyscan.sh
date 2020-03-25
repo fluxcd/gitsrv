34b4bc481e41149ddc64bf9d52a3c01afb9bd59b
stefanprodan <stefan.prodan@gmail.com>
2020-03-25 15:01:37 +0200

    ci: upload known_hosts to GH artifacts

--- test/ssh-keyscan.sh
+++ test/ssh-keyscan.sh
@@ -3,5 +3,5 @@
 repo_root=$(git rev-parse --show-toplevel)
 gen_dir=$(mktemp -d)
 
-kubectl exec -it deployment/gitsrv ssh-keyscan gitsrv > "$gen_dir/known_hosts"
-cat "$gen_dir/known_hosts" |  grep gitsrv
+kubectl exec -it deployment/gitsrv ssh-keyscan gitsrv > known_hosts.txt
+cat known_hosts.txt | grep gitsrv
