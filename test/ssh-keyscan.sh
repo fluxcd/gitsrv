72f28a1e3b67d13262a47e61112b70250f92630d
stefanprodan <stefan.prodan@gmail.com>
2020-03-25 18:15:45 +0200

    build: remove interactive mode

--- test/ssh-keyscan.sh
+++ test/ssh-keyscan.sh
@@ -2,5 +2,5 @@
 
 repo_root=$(git rev-parse --show-toplevel)
 
-kubectl exec -it deployment/gitsrv ssh-keyscan gitsrv > known_hosts.txt
+kubectl exec deployment/gitsrv ssh-keyscan gitsrv > known_hosts.txt
 grep -E gitsrv known_hosts.txt
